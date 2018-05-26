6939d0ae99f58b70dfa3e72838e87ebe19dd72be
a4geru <is0248vx@ed.ritsumei.ac.jp>
2018-05-26 15:22:48 +0000

    initial commit

---
+++ Gemfile
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+# gem "rails"
+gem 'line-bot-api'
+gem 'sinatra'
+gem 'dotenv'
+
+group :test do
+  gem 'rspec'
+  gem 'rack-test'
+  gem 'pry'
+  gem 'guard'
+  gem 'guard-rspec', require: false
+  gem 'guard-rubocop', require: false
+end
---
+++ spec/app_spec.rb
@@ -0,0 +1,24 @@
+
+require './app'
+require './spec/spec_helper'
+
+describe "My Sinatra Application" do
+  before do
+    module RSpecMixin
+      include Rack::Test::Methods
+      def app() Sinatra::Application end
+    end
+    
+    RSpec.configure { |c| c.include RSpecMixin } 
+  end
+
+  it "should allow accessing the main page" do
+    get '/'
+    expect(last_response).to be_ok
+  end
+
+  it "should allow accessing the callback page" do
+    post '/callback'
+    expect(last_response).to be_bad_request
+  end
+end
